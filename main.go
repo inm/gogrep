4e75aa4c8915d24da27511ab526affe119a6b31e
Martin Drlik <martadrlik@gmail.com>
2014-09-18 14:10:58 +0200

    line number, invert match

--- main.go
+++ main.go
@@ -5,17 +5,76 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
 	"regexp"
 )
 
+var options = struct {
+	help      *bool
+	n         *bool
+	recursive *bool
+	v         *bool
+}{
+	help:      flag.Bool("help", false, "display this help and exit"),
+	n:         flag.Bool("n", false, "print line number with output lines"),
+	recursive: flag.Bool("r", false, "handle directories recusively"),
+	v:         flag.Bool("v", false, "select non-matching lines"),
+}
+
+type Match struct {
+	Content string
+	Number  int
+}
+
 var (
-	help = flag.Bool("help", false, "display this help and exit")
+	exitCode int
 )
+
+func match(pattern *regexp.Regexp, path string, num int, line string) {
+	if !*options.v == pattern.MatchString(line) {
+		reportMatch(path, num, line)
+	}
+}
+
+func searchFile(pattern *regexp.Regexp, path string) error {
+	f, err := os.OpenFile(path, os.O_RDONLY, 0)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	scan := bufio.NewScanner(f)
+	num := 0
+	for scan.Scan() {
+		num++
+		match(pattern, path, num, scan.Text())
+	}
+	return scan.Err()
+}
+
+func reportError(err error) {
+	switch err := err.(type) {
+	case *os.PathError:
+		fmt.Fprintf(os.Stderr, "gogrep: %s: %s\n", err.Path, err.Err)
+	default:
+		fmt.Fprintln(os.Stderr, "gogrep:", err)
+	}
+	exitCode = 2
+}
+
+func reportMatch(path string, num int, line string) {
+	if path != "" {
+		fmt.Print(path, ":")
+	}
+	if *options.n {
+		fmt.Print(num, ":")
+	}
+	fmt.Println(line)
+}
 
 func main() {
 	flag.Parse()
 
-	if *help {
+	if *options.help {
 		fmt.Println("Usage: gogrep [OPTION]... PATTERN [FILE]...")
 		fmt.Println("Search for PATTERN in each FILE or standard input.")
 		fmt.Println("Example: gogrep -i 'hello world' menu.h main.c")
@@ -38,35 +97,49 @@ func main() {
 
 	if flag.NArg() == 1 {
 		scan := bufio.NewScanner(os.Stdin)
+		num := 0
 		for scan.Scan() {
-			t := scan.Text()
-			if pattern.MatchString(t) {
-				fmt.Println(t)
-			}
+			num++
+			match(pattern, "", num, scan.Text())
+		}
+		if err := scan.Err(); err != nil {
+			reportError(err)
 		}
 		return
 	}
 
 	for i := 1; i < flag.NArg(); i++ {
-		filename := flag.Arg(i)
-		file, err := os.Open(filename)
+		filenames, err := filepath.Glob(flag.Arg(i))
 		if err != nil {
-			// test me
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(2)
+			reportError(err)
 		}
-		defer file.Close()
-		scan := bufio.NewScanner(file)
-		for scan.Scan() {
-			t := scan.Text()
-			if pattern.MatchString(t) {
-				fmt.Println(t)
+		for _, name := range filenames {
+			info, err := os.Stat(name)
+			if err != nil {
+				reportError(err)
+			}
+			if info.IsDir() {
+				err = filepath.Walk(name, func(path string, info os.FileInfo, err error) error {
+					if err != nil {
+						return err
+					}
+					if info.IsDir() {
+						if !*options.recursive {
+							return filepath.SkipDir
+						}
+						return nil
+					}
+					return searchFile(pattern, path)
+				})
+				if err != nil {
+					reportError(err)
+				}
+				continue
+			}
+			if err := searchFile(pattern, name); err != nil {
+				reportError(err)
 			}
 		}
-		if err := scan.Err(); err != nil {
-			// test me
-			fmt.Fprintln(os.Stderr, err)
-			os.Exit(2)
-		}
 	}
+	os.Exit(exitCode)
 }
